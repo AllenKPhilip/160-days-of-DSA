3afa0352aaffe31ca81fe18a3aa394ca143a73bc
ALLEN K PHILIP <145896298+AllenKPhilip@users.noreply.github.com>
2025-01-26 17:29:47 +0530

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,20 @@
+"""
+Author:ALLEN K PHILIP
+Description:an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+"""
+
+def sort012(arr):
+    c0 = 0
+    c1 = 0
+    c2 = 0
+
+    # Count 0s, 1s and 2s
+    for num in arr:
+        if num == 0:
+            c0 += 1
+        elif num == 1:
+            c1 += 1
+        else:
+            c2 += 1
+
+    idx = 0
