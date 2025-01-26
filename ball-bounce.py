6fb5d09e808d8bfcd35fffc3b44e9b29bede58ac
ALLEN K PHILIP <145896298+AllenKPhilip@users.noreply.github.com>
2025-01-26 17:50:25 +0530

    Create ball-bounce.py

---
+++ ball-bounce.py
@@ -0,0 +1,14 @@
+"""
+Author:ALLEN K PHILIP
+Description:Program that let's the user enter to initial height of the ball. show the total distance the ball bounces and total bounce.
+"""
+
+height=int(input("Enter the height that ball dropped(in CentiMetre):"))
+distance=height
+bounce=1
+while height>=1:
+    height=height*.6
+    distance+=height
+    bounce+=1
+print("Total distance:",distance,"cm")
+print("Total bounce:",bounce)
